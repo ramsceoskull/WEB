508c7af917aa363368a17ce5e7b8edd662cf2172
Ramsés Anceno <ramses.social0@gmail.com>
2024-03-06 01:18:21 -0600

    Add an a cliente php script

--- php/registerUser.php
+++ php/registerUser.php
@@ -1,4 +1,16 @@
 <?php
 	include "./connection.php";
 
-	// $name
+	$name = $_POST['nombre'];
+	$age = $_POST['age'];
+	$apellidoP = $_POST['apellidoP'];
+	$apellidoM = $_POST['apellidoM'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+	$sql = mysqli_query($connection, "INSERT INTO Usuario(id, nombres, edad, apellidoPaterno, apellidoMaterno, correo, pass) VALUES (0, '$name', '$age', '$apellidoP', '$apellidoM', '$email', '$password')");
+
+	if($sql)
+		echo " -> Usuario registrado";
+	else
+		echo " -> Error al registrar usuario";

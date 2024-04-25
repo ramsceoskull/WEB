8f5c806b2639cf7237c97f2bb242f32b4fe101b1
Ramsés Anceno <ramses.social0@gmail.com>
2024-04-25 03:51:51 -0600

    mobile index were finished (design)

--- script.js
+++ script.js
@@ -15,3 +15,51 @@ window.addEventListener('scroll', function () {
 	// console.log(this.window.scrollY);
 	prevY = window.scrollY;
 });
+
+
+const handleChangeReview = () => {
+	for (let item of document.querySelectorAll(".review")) {
+		item.classList.toggle("hidden");
+	}
+	document.querySelector(".fa-angle-right").classList.toggle("invisible");
+	document.querySelector(".fa-angle-left").classList.toggle("invisible");
+};
+
+const mobileReviewContainer = () => {
+	document.querySelector(".fa-angle-left").classList.remove("hidden");
+	document.querySelector(".fa-angle-right").classList.remove("hidden");
+	document.querySelector(".fa-angle-left").classList.add("invisible");
+	document.querySelector(".fa-angle-right").classList.remove("invisible");
+	document.getElementById("1").classList.remove("hidden");
+	// document.getElementById("2").classList.add("hidden");
+	document.getElementById("3").classList.add("hidden");
+
+	// let plan = document.getElementsByClassName("plans-card")[1];
+	// plan.scrollIntoView({ inline: "center" });
+};
+
+const tabletReviewContainer = () => {
+	document.querySelector(".fa-angle-right").classList.add("invisible");
+	document.querySelector(".fa-angle-left").classList.add("invisible");
+	document.getElementById("1").classList.remove("hidden");
+	// document.getElementById("2").classList.remove("hidden");
+	document.getElementById("3").classList.remove("hidden");
+};
+
+window.addEventListener("load", () => {
+	const media = window.matchMedia(`(min-width: 768px)`);
+
+	media.addEventListener("change", (e) => {
+		if (e.matches) {
+			tabletReviewContainer();
+		} else {
+			mobileReviewContainer();
+		}
+	});
+
+	if (media.matches) {
+		tabletReviewContainer();
+	} else {
+		mobileReviewContainer();
+	}
+});
